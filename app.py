58e25f01aa8a9869912b8b4506975e25f3ef2e3c
Dominik Tuchyna <dtuchyna@redhat.com>
2020-07-07 14:10:11 +0200

    Add scheduler, configmap and pipfiles [8pt] (#1)
    * Add scheduler, configmap and pipfiles
    
    * Use thoth.common w/ srcops mthds
    
    * Edit confmap description & configuration
    
    * Add click for cli
    
    * Implement scheduling only in app.py
    
    * remove schedule class
    
    * add checks for empty args
    
    * change list to empty str
    
    * remove lst comprehension
    
    * Add python project properties
    
    * Add openshift templates
    
    * Change cli to main and add exc. handling
    
    * Skip archived repositories
    
    * Move templates to thoth-application
    
    * Downgrade python version
    
    * Make setup.py use version.py
    
    * Satisfy pre-commit
    
    * add license header and docstrings
    
    * Make pre-commit fix files
    
    * Use logger
    
    * Remove init
    
    * Remove setup.py
    
    * Move version & title to app.py
    
    * Add thoth init logging
    
    * Avoid duplicate repos
    
    * Remove duplicates from params

--- app.py
+++ app.py
@@ -17,7 +17,72 @@
 
 """This is the main script of the template project."""
 
-from template.version import __version__
+from typing import Optional, Set
+
+import click
+import logging
+from github import Github
+from github.GithubException import GithubException
+from thoth.common import OpenShift
+
+from thoth.common import init_logging
+
+__title__ = "thoth.mi-scheduler"
+__version__ = "0.1.0"
+
+init_logging()
+_LOGGER = logging.getLogger(__title__)
+
+
+@click.command()
+@click.option(
+    "--repositories", help="Repositories to be analysed (e.g thoth-station/performance)",
+)
+@click.option(
+    "--organizations",
+    "-o",
+    type=str,
+    required=False,
+    help="Organizations (all of their repositories) to be analysed (e.g. AICoE)",
+)
+def main(
+    repositories: Optional[str], organizations: Optional[str],
+):
+    """Command Line Interface for SrcOpsMetrics."""
+    gh = Github()
+
+    orgs = organizations.split(",") if organizations is not None else []
+
+    repos = set()
+    for org in orgs:
+        try:
+            gh_org = gh.get_organization(org)
+            for repo in gh_org.get_repos():
+                if repo.archived:
+                    _LOGGER.info("repository %s is archived, therefore skipped" % repo.full_name)
+                else:
+                    repos.add(repo.full_name)
+        except GithubException:
+            _LOGGER.error("organization %s was not recognized by GitHub API" % org)
+
+    repos.union(repositories.split(",") if repositories is not None else [])
+
+    schedule_repositories(repositories=repos)
+
+
+def schedule_repositories(repositories: Set[str]) -> None:
+    """Schedule workflows for repositories.
+
+    Repositories are also gathered from all of the organizations passed.
+
+    :param organizations:str: List of organizations in string format: org1,org2,org3,...
+    :param repositories:str: List of repositories in string format: repo1,repo2,...
+    """
+    oc = OpenShift()
+    for repo in repositories:
+        oc.schedule_srcopsmetrics(repository=repo)
+
 
 if __name__ == "__main__":
-    print(f"A template project with Thoth integration, v{__version__}.")
+    _LOGGER.info("mi-scheduler for scheduling mi workflows v%s", __version__)
+    main()
