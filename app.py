1b52fc92424059fa4d8ca566093a3903758e0910
Dominik Tuchyna <dtuchyna@redhat.com>
2021-01-20 21:35:23 +0100

    Add kebechet schedule, implement class Schedule (#58)
    * Add kebechet schedule, implement class Schedule
    
    * Uncomment, remove raise NotImplementedError
    
    * Get active kebechet repos, add thoth-storages
    
    * Be consistent with storages method
    
    * Remove openshift variables

--- app.py
+++ app.py
@@ -17,15 +17,19 @@
 
 """This is the main script of the template project."""
 
-from typing import Set
+from typing import Set, List
 
 import logging
 import os
+
 from github import Github
+from github.Repository import Repository
+
 from github.GithubException import UnknownObjectException
 from thoth.common import OpenShift
-
 from thoth.common import init_logging
+
+from thoth.storages import GraphDatabase
 
 __title__ = "thoth.mi-scheduler"
 __version__ = "1.0.7"
@@ -35,48 +39,80 @@ _LOGGER = logging.getLogger(__title__)
 
 GITHUB_ACCESS_TOKEN = os.getenv("GITHUB_ACCESS_TOKEN")
 
+KEBECHET_ENTITIES = "KebechetUpdateManager,DependencyUpdate"
+
+
+class Schedule:
+    """Schedule class which handles repository and organization checks and schedule methods."""
+
+    def __init__(self, github: Github, organizations: List[str] = None, repositories: List[str] = None):
+        """Initialize with github, orgs and repos optional."""
+        self.gh = github
+        self.orgs = organizations
+        self.repos = repositories
+        self.github_repos: Set[Repository] = set()
+
+        self._initialize_repositories_from_organizations()
+        self._initialize_repositories_from_raw()
+
+    def _initialize_repositories_from_organizations(self):
+        """Check organizations if exist and acquire their repositories."""
+        for org in self.orgs:
+            try:
+                gh_org = self.gh.get_organization(org)
+                for repo in gh_org.get_repos():
+                    if repo.archived:
+                        _LOGGER.info("repository %s is archived, therefore skipped", repo.full_name)
+                        continue
+
+                    if repo.full_name in self.repos:
+                        self.repos.remove(repo.full_name)
+
+                    self.github_repos.add(repo)
+
+            except UnknownObjectException:
+                _LOGGER.error("organization %s was not recognized by GitHub API", org)
+
+    def _initialize_repositories_from_raw(self):
+        """Check repositories if exist and if not archived."""
+        for repo in self.repos:
+            try:
+                gh_repo = self.gh_get_repo(repo)
+
+                if gh_repo.archived:
+                    _LOGGER.info("repository %s is archived, therefore skipped", repo.full_name)
+                    continue
+
+                self.github_repos.add(gh_repo)
+
+            except UnknownObjectException:
+                _LOGGER.error("Repository %s was not recognized by GitHub API", repo)
+
+    def schedule_for_mi_analysis(self) -> None:
+        """Schedule workflows for mi analysis."""
+        for repo in self.github_repos:
+            workflow_id = OpenShift().schedule_mi_workflow(repository=repo.full_name)
+            _LOGGER.info("Scheduled mi with id %r", workflow_id)
+
+    def schedule_for_kebechet_analysis(self):
+        """Schedule workflows for kebechet analysis."""
+        for repo in self.github_repos:
+            workflow_id = OpenShift().schedule_mi_workflow(repository=repo.full_name, entities=KEBECHET_ENTITIES)
+            _LOGGER.info("Scheduled mi-kebechet analysis with id %r", workflow_id)
+
 
 def main():
     """MI-Scheduler entrypoint."""
     gh = Github(login_or_token=GITHUB_ACCESS_TOKEN)
 
-    repos_raw, orgs = OpenShift().get_mi_repositories_and_organizations()
-    repos = set()
+    repos, orgs = OpenShift().get_mi_repositories_and_organizations()
+    Schedule(gh, orgs, repos).schedule_for_mi_analysis()
 
-    for org in orgs:
-        try:
-            gh_org = gh.get_organization(org)
-            for repo in gh_org.get_repos():
-                if repo.archived:
-                    _LOGGER.info("repository %s is archived, therefore skipped", repo.full_name)
-                else:
-                    repos.add(repo.full_name)
-        except UnknownObjectException:
-            _LOGGER.error("organization %s was not recognized by GitHub API", org)
-
-    for repo in repos_raw:
-        try:
-            if gh.get_repo(repo).archived:
-                _LOGGER.info("repository %s is archived, therefore skipped", repo.full_name)
-            else:
-                repos.add(repo)
-        except UnknownObjectException:
-            _LOGGER.error("Repository %s was not recognized by GitHub API", repo)
-
-    schedule_repositories(repositories=repos)
-
-
-def schedule_repositories(repositories: Set[str]) -> None:
-    """Schedule workflows for repositories.
-
-    Repositories are also gathered from all of the organizations passed.
-
-    :param repositories:str: List of repositories in string format: repo1,repo2,...
-    """
-    oc = OpenShift()
-    for repo in repositories:
-        workflow_id = oc.schedule_mi_workflow(repository=repo)
-        _LOGGER.info("Scheduled mi with id %r", workflow_id)
+    graph = GraphDatabase()
+    graph.connect()
+    kebechet_repos = graph.get_active_kebechet_github_installations_repos()
+    # TODO use the return value more efficiently to assign only active managers
+    Schedule(gh, repositories=kebechet_repos).schedule_for_kebechet_analysis(kebechet_repos)
 
 
 if __name__ == "__main__":
