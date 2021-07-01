57f9d391b8fbd4f292dced92afcc6a297aba0704
Dominik Tuchyna <dtuchyna@redhat.com>
2021-07-01 13:51:27 +0200

    Add deployment_name from envvar and component name for mi analysis

--- app.py
+++ app.py
@@ -60,9 +60,9 @@ class Schedule:
 
         self.checked_repos: Set[str] = set()
 
-        deployment_name = self.oc.infra_namespace  # mi runs in infra (submit_mi() in workflows.py in common)
+        deployment_name = os.environ["THOTH_DEPLOYMENT_NAME"]
         self.kebechet_path = Path(f"{deployment_name}/{subdir}/thoth-sli-metrics/kebechet-update-manager/")
-        self.mi_path = Path(f"{deployment_name}/{subdir}/mi/")
+        self.mi_path = Path(f"{deployment_name}/mi/{subdir}")
 
         self._initialize_repositories_from_organizations()
         self._initialize_repositories_from_raw()
